f00c3d98ac116247488163cd4a4b9c54624f4ff5
Sran012 <88543485+Sran012@users.noreply.github.com>
2026-03-02 11:21:08 +0000

    revive protected route for dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -63,9 +63,9 @@ function App() {
           <Route
             path="/admin/dashboard"
             element={
-              // <ProtectedRoute>
+              <ProtectedRoute>
                 <Dashboard />
-              /* </ProtectedRoute> */
+              </ProtectedRoute>
             }
           />
           
